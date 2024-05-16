57f873763081199aa615a3bbcf2af27d73c98da2
Iren <aquasport@mail.ru>
2024-05-16 19:40:21 +0300

    deleted text, add module string, add function multy

--- first_git/simple_func.py
+++ first_git/simple_func.py
@@ -1,9 +1,13 @@
 def plus(a, b):
     return a + b
-text = 'New text'
+
 def minus(a, b):
     return a - b
+
+def multy(a, b):
+    return a * b
 
 if __name__ == '__main__':
     print(plus(2, 3))
     print(minus(7, 5))
+    print(multy(5, 5))
